1d6c00140229aa25bb6f8299bdf3a17d4984ee52
준모 <@pallen-pc>
2018-05-11 17:11:49 +0900

    lombok 추가

--- study/src/main/java/sy/bean/MemberDto.java
+++ study/src/main/java/sy/bean/MemberDto.java
@@ -3,6 +3,9 @@ package sy.bean;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import lombok.Data;
+
+@Data
 public class MemberDto {
 
 	private String email;
@@ -10,8 +13,6 @@ public class MemberDto {
 	private String pw;
 	private String grade;
 	private String reg;
-	
-	
 	
 	public MemberDto(ResultSet rs) throws SQLException {
 		setEmail(rs.getString("email"));
@@ -25,35 +26,6 @@ public class MemberDto {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getPw() {
-		return pw;
-	}
-	public void setPw(String pw) {
-		this.pw = pw;
-	}
-	public String getGrade() {
-		return grade;
-	}
-	public void setGrade(String grade) {
-		this.grade = grade;
-	}
-	public String getReg() {
-		return reg;
-	}
-	public void setReg(String reg) {
-		this.reg = reg;
-	}
+	
 	
 }
